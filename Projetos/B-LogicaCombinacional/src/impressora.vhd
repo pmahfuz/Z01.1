d07e6c7da5564d5002764c48dd865a925efa156b
Sarah <sarahapc@al.insper.edu.br>
2021-09-06 21:11:11 -0300

    impressora

--- Projetos/B-LogicaCombinacional/src/impressora.vhd
+++ Projetos/B-LogicaCombinacional/src/impressora.vhd
@@ -11,5 +11,20 @@ architecture arch of impressora is
 
 begin
 
+    precess(SW1,SW2,SW3,SW4)
+    begin 
+        if (SW1 = '1' and SW2 = '1') then 
+            x <= '1'
+        elsif (SW1 = '1' and SW3 = '1') then 
+            x <= '1'
+        elsif (SW2 = '1' and SW3 = '1') then 
+            x <= '1'
+        elsif (SW2 = '1' and SW4 = '1') then 
+            x <= '1'
+        elsif (SW3 = '1' and SW4 = '1') then 
+            x <= '1' 
+              
+        end if;
+    end process
 
 end architecture;
