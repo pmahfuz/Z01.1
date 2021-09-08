a33bdedf9e90aece48c9ae85523e59a99a126bcb
Sarah <sarahapc@al.insper.edu.br>
2021-09-08 11:51:37 -0300

    alteracoes sevenSeg

--- Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
@@ -9,19 +9,17 @@ end entity;
 
 architecture arch of sevenSeg is
 begin
-	
-	case bcd is
-		when "0000" => sevenSeg <= "0000001"  -- 0
-		when "0001" => sevenSeg <= "1001111"  -- 1
-		when "0010" => sevenSeg <= "0010010"  -- 2
-		when "0011" => sevenSeg <= "0000111"  -- 3
-		when "0100" => sevenSeg <= "1001100"  -- 4
-		when "0101" => sevenSeg <= "0100100"  -- 5
-		when "0110" => sevenSeg <= "0100000"  -- 6
-		when "0111" => sevenSeg <= "0001111"  -- 7
-		when "1000" => sevenSeg <= "0000000"  -- 8
-		when "1001" => sevenSeg <= "0001100"  -- 9
-		when others => sevenSeg <= "1111111"  -- null
-	end case
+with bcd select
+	leds <= "0000001" when "0000",   -- 0
+			"1001111" when "0001",   -- 1
+			"0010010" when "0010",   -- 2
+			"0000111" when "0011",   -- 3
+			"1001100" when "0100",   -- 4
+			"0100100" when "0101",   -- 5
+			"0100000" when "0110",   -- 6
+			"0001111" when "0111",   -- 7
+			"0000000" when "1000",   -- 8
+			"0001100" when "1001",   -- 9
+			"1111111" when others;   -- null
 
 end architecture;
