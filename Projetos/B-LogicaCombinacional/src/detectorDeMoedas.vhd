36870a691657df7dc1b9db9c701c9e8356c17198
Sarah <sarahapc@al.insper.edu.br>
2021-09-08 14:07:38 -0300

    mudanca detector de moeda

--- Projetos/B-LogicaCombinacional/src/detectorDeMoedas.vhd
+++ Projetos/B-LogicaCombinacional/src/detectorDeMoedas.vhd
@@ -10,15 +10,15 @@ end entity;
 architecture arch of detectorDeMoedas is
 
 begin
-    precess(Q,D,N)
+    process(Q,D,N)
     begin 
         if (Q = '1') then 
-            cents <= '11001'
+            cents <= "11001";
         elsif (D = '1') then 
-            cents <= '01010'
+            cents <= "01010";
         elsif (N = '1') then
-            cents <= '00101'
+            cents <= "00101";
         end if;
-    end process
+    end process;
 
 end architecture;
