a84163235a4382772b19230aab15205fef234e78
lorenabc <lorenabcar@gmail.com>
2021-09-09 19:56:37 -0300

    conceito A

--- Projetos/B-LogicaCombinacional/src/ConceitoA.vhd
+++ Projetos/B-LogicaCombinacional/src/ConceitoA.vhd
@@ -24,15 +24,72 @@ end entity;
 -- Implementacao do bloco --
 ----------------------------
 architecture rtl of ConceitoA is
+component binarioToBcd is
+  generic(N: positive := 10);
+    port(
+        clk, reset: in std_logic;
+        binary_in: in std_logic_vector(N-1 downto 0);
+        bcd0, bcd1, bcd2, bcd3, bcd4: out std_logic_vector(3 downto 0)
+    );
+end component;
 
 --------------
 -- signals
 --------------
 
+signal bcd0_hex : std_logic_vector(3 downto 0);
+signal bcd1_hex : std_logic_vector(3 downto 0);
+signal bcd2_hex : std_logic_vector(3 downto 0);
+
+begin
+
+u1: binarioToBcd port map(
+		clk => CLOCK_50,
+		reset => '0',
+      binary_in => SW,
+      bcd0 => bcd0_hex,
+		bcd1 => bcd1_hex,
+		bcd2 => bcd2_hex);
+		
 ---------------
 -- implementacao
 ---------------
-begin
 
+with bcd2_hex select
+		HEX2 <= "0000001" when "0000", --numero 0 
+				  "1001111" when "0001", --numero 1
+				  "0010010" when "0010", --numero 2
+				  "0000110" when "0011", --numero 3
+				  "1001100" when "0100", --numero 4
+				  "0100100" when "0101", --numero 5
+				  "0100000" when "0110", --numero 6
+				  "0001111" when "0111", --numero 7
+				  "1111111" when "1000", --numero 8
+				  "0000100" when others;
+
+with bcd1_hex select
+		HEX1 <= "0000001" when "0000", --numero 0 
+				  "1001111" when "0001", --numero 1
+				  "0010010" when "0010", --numero 2
+				  "0000110" when "0011", --numero 3
+				  "1001100" when "0100", --numero 4
+				  "0100100" when "0101", --numero 5
+				  "0100000" when "0110", --numero 6
+				  "0001111" when "0111", --numero 7
+				  "1111111" when "1000", --numero 8
+				  "0000100" when others;
+
+
+with bcd0_hex select
+		HEX0 <= "0000001" when "0000", --numero 0 
+				  "1001111" when "0001", --numero 1
+				  "0010010" when "0010", --numero 2
+				  "0000110" when "0011", --numero 3
+				  "1001100" when "0100", --numero 4
+				  "0100100" when "0101", --numero 5
+				  "0100000" when "0110", --numero 6
+				  "0001111" when "0111", --numero 7
+				  "1111111" when "1000", --numero 8
+				  "0000100" when others;
 
 end rtl;
