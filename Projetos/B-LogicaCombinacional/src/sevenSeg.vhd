20e0ab4d2f201de164a239067d1731eff74ac6df
Sarah <sarahapc@al.insper.edu.br>
2021-09-13 19:58:16 -0300

    arrumando erros

--- Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
@@ -9,17 +9,17 @@ end entity;
 
 architecture arch of sevenSeg is
 begin
-with bcd select
-	leds <= "0000001" when "0000",   -- 0
-			"1001111" when "0001",   -- 1
-			"0010010" when "0010",   -- 2
-			"0000111" when "0011",   -- 3
-			"1001100" when "0100",   -- 4
-			"0100100" when "0101",   -- 5
-			"0100000" when "0110",   -- 6
-			"0001111" when "0111",   -- 7
-			"0000000" when "1000",   -- 8
-			"0001100" when "1001",   -- 9
-			"1111111" when others;   -- null
+	with bcd select
+		leds <= "0000001" when "0000",   -- 0
+					"1001111" when "0001", -- Número 1 
+					"0010010" when "0010", -- Número 2
+					"0000110" when "0011", -- Número 3
+					"1001100" when "0100", -- Número 4
+					"0100100" when "0101", -- Número 5
+					"0100000" when "0110", -- Número 6
+					"0001111" when "0111", -- Número 7
+					"0000000" when "1000", -- Número 8
+					"0000100" when "1001", -- Número 9
+					"1111111" when others;   -- null
 
 end architecture;
